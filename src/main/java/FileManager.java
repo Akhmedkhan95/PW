71bf5820c4020379b79bbbbfc0346f105d7f552a
Arsakhanov Akhmedkhan <amahan.ru@gmail.com>
2025-05-31 08:34:17 +0300

    init PW

---
+++ src/main/java/FileManager.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.nio.file.*;
+import java.nio.file.attribute.*;
+import java.text.SimpleDateFormat;
+import java.util.*;
+import java.util.stream.*;
+
+public class FileManager {
+    private static Path currentPath = Paths.get("").toAbsolutePath();
+    private static final Scanner scanner = new Scanner(System.in);
+    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+    public static void main(String[] args) {
+
+    }
+}
